9995328748d55a0a8fb4849df30d63921157f2f3
Deborah Shapiro <deborah.seider.shapiro@gmail.com>
2015-09-22 13:20:02 -0700

    ValidationClientRest.java implementation. Initial users for sample-data mojo.

--- admin/src/main/java/org/ihtsdo/otf/refset/mojo/GenerateSampleDataMojo.java
+++ admin/src/main/java/org/ihtsdo/otf/refset/mojo/GenerateSampleDataMojo.java
@@ -19,11 +19,19 @@
  */
 package org.ihtsdo.otf.refset.mojo;
 
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoFailureException;
+import org.ihtsdo.otf.refset.User;
 import org.ihtsdo.otf.refset.helpers.ConfigUtility;
+import org.ihtsdo.otf.refset.jpa.ProjectJpa;
+import org.ihtsdo.otf.refset.jpa.UserJpa;
+import org.ihtsdo.otf.refset.jpa.UserPreferencesJpa;
+import org.ihtsdo.otf.refset.rest.client.ProjectClientRest;
+import org.ihtsdo.otf.refset.rest.client.SecurityClientRest;
 
 /**
  * Goal which generates sample data in an empty database. Uses JPA services
@@ -36,6 +44,34 @@ import org.ihtsdo.otf.refset.helpers.ConfigUtility;
  */
 public class GenerateSampleDataMojo extends AbstractMojo {
 
+  /**  The project. */
+  ProjectClientRest project = null;
+  
+  /** The project1. */
+  ProjectJpa project1 = null;
+
+  /** The project2. */
+  ProjectJpa project2 = null;
+  
+  /**  The admin1 token. */
+  String admin1Token = "";
+  
+  /**  The admin2 token. */
+  String admin2Token = "";
+  
+  /**  The viewer token. */
+  String viewerToken = "";
+  
+  /**  The author1 token. */
+  String author1Token = "";
+  
+  /**  The author2 token. */
+  String author2Token = "";
+  
+  /**  The author3 token. */
+  String author3Token = "";
+  
+  
   /**
    * Instantiates a {@link GenerateSampleDataMojo} from the specified
    * parameters.
@@ -45,9 +81,44 @@ public class GenerateSampleDataMojo extends AbstractMojo {
   }
 
   /* see superclass */
-  @SuppressWarnings("unused")
+  /* (non-Javadoc)
+   * @see org.apache.maven.plugin.Mojo#execute()
+   */
   @Override
   public void execute() throws MojoFailureException {
+    try {
+      getLog().info("Sample data generation called via mojo.");
+
+      boolean serverRunning = ConfigUtility.isServerActive();
+      getLog().info(
+          "Server status detected:  " + (!serverRunning ? "DOWN" : "UP"));
+      if (!serverRunning) {
+        throw new Exception("Server must be running");
+      }
+
+      getLog().info("Running against server");
+
+      loadSampleData();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new MojoFailureException("Unexpected exception:", e);
+    }
+
+  }
+
+  /**
+   * Load sample data.
+   *
+   * @throws Exception the exception
+   */
+  /**
+   * Load sample data.
+   *
+   * @throws Exception the exception
+   */
+  private void loadSampleData() throws Exception {
+    
     getLog().info("Start generating sample data");
 
     try {
@@ -55,11 +126,13 @@ public class GenerateSampleDataMojo extends AbstractMojo {
       Properties properties = ConfigUtility.getConfigProperties();
 
       // TODO:
-      // Create users
-      // Create projects
-      // Create refsets
-      // Create Translations
-      //
+      /*Create users (author1, author2, author3, lead1, lead2, lead3, admin1, admin 2)
+      Create  projects (project 1, project 2)
+      Assign users to project (different roles)
+          project 1 -> author1, author2, lead1, admin 1
+          project 2 -> author 3, lead2, lead3, admin 2
+      Create refsets
+      Create translations*/
 
       //
       // consider different versions of same edition
@@ -69,7 +142,97 @@ public class GenerateSampleDataMojo extends AbstractMojo {
       // ..etc
       // consider intensional, extensional, external
       //
+      final SecurityClientRest security = new SecurityClientRest(properties);
+      viewerToken = security.authenticate("guest", "guest").getAuthToken();
+      author1Token =
+          security.authenticate("author1", "author1").getAuthToken();
+      author2Token =
+          security.authenticate("author2", "author2").getAuthToken();
+      author3Token =
+          security.authenticate("author3", "author3").getAuthToken();
+      //leadToken = security.authenticate("demo_lead", "demo_lead").getAuthToken();
+      admin1Token = security.authenticate("admin1", "admin1").getAuthToken();
+      admin2Token = security.authenticate("admin2", "admin2").getAuthToken();
+      
+      final User viewer = security.getUser("guest", viewerToken);
+      getLog().info("  viewer = " + viewer);
+      
+      final User author1 = security.getUser("author1", author1Token);
+      author1.setEmail("***REMOVED***");
+      security.updateUser((UserJpa) author1, admin1Token);
+      getLog().info("  author1 user = " + author1);
+      final User author2 = security.getUser("author2", author2Token);
+      author2.setEmail("***REMOVED***");
+      security.updateUser((UserJpa) author2, admin1Token);
+      getLog().info("  author2 user = " + author2);
+      final User author3 = security.getUser("author3", author3Token);
+      author3.setEmail("***REMOVED***");
+      security.updateUser((UserJpa) author3, admin1Token);
+      getLog().info("  author3 user = " + author3);
+      //final User lead = security.getUser("demo_lead", leadToken);
+      //getLog().info("  lead = " + lead);
+      
+      final User admin1 = security.getUser("admin1", admin1Token);
+      getLog().info("  admin1 = " + admin1);
 
+      project = new ProjectClientRest(properties);
+      
+      // Add user preferences for all users
+      // Default notification preferences.
+      final UserPreferencesJpa viewerPrefs = new UserPreferencesJpa();
+      viewerPrefs.setUser(viewer);
+      //project.addUserPreferences(viewerPrefs, admin1Token);
+      getLog().info("  viewerPrefs = " + viewerPrefs);
+      final UserPreferencesJpa authorPrefs = new UserPreferencesJpa();
+      authorPrefs.setUser(author1);
+      
+      // set default notification prefs to send ALERTS
+      // on ERROR types
+      /*NotificationPreferences standardNotificationPrefs =
+          new NotificationPreferencesJpa();
+      standardNotificationPrefs
+          .setNotificationChannelType(NotificationChannelType.ALERTS);
+      standardNotificationPrefs.setNotifyOfMeasurement(true);
+      standardNotificationPrefs.addEnabledType(TelemetryEventType.ERROR);
+      standardNotificationPrefs.addEnabledType(TelemetryEventType.INFO);
+      standardNotificationPrefs.setNotifyOfMeasurement(true);
+      standardPrefs.setDefaultNotificationPreferences(standardNotificationPrefs);*/
+
+      //project.addUserPreferences(authorPrefs, admin1Token);
+      getLog().info("  standardPrefs = " + authorPrefs);
+      /*final UserPreferencesJpa leadPrefs = new UserPreferencesJpa();
+      leadPrefs.setUser(lead1);
+      //project.addUserPreferences(leadPrefs, admin1Token);
+      getLog().info("  leadPrefs = " + leadPrefs);*/
+      final UserPreferencesJpa adminPrefs = new UserPreferencesJpa();
+      adminPrefs.setUser(admin1);
+      //project.addUserPreferences(adminPrefs, admin1Token);
+      getLog().info("  adminPrefs = " + adminPrefs);
+
+      project1 = new ProjectJpa();
+      project1.setId(1L);
+      //project1.setAdminNotification(true);
+      final Set<User> authors = new HashSet<>();
+      authors.add(author1);
+      authors.add(author2);
+      project1.setAuthors(authors);
+      //project1.setStandardUserNotification(true);
+      /*final Set<User> leads = new HashSet<>();
+      leads.add(lead1);
+      project1.setLeads(leads);
+      //project1.setLeadNotification(true);
+*/    final Set<User> admins = new HashSet<>();
+      admins.add(admin1);
+      project1.setAdmins(admins);
+      project1.setName("SNOMED CT Release Service");
+      project1
+          .setDescription("Represents activities related to the production of beta and official SNOMED releases.");
+      project1 = (ProjectJpa) project.addProject(project1, admin1Token);
+      getLog().info("  project1 = " + project1);
+
+      
+      
+      
       getLog().info("Done ...");
     } catch (Exception e) {
       e.printStackTrace();
--- rest-client/src/main/java/org/ihtsdo/otf/refset/rest/client/ValidationClientRest.java
+++ rest-client/src/main/java/org/ihtsdo/otf/refset/rest/client/ValidationClientRest.java
@@ -5,7 +5,26 @@ package org.ihtsdo.otf.refset.rest.client;
 
 import java.util.Properties;
 
+
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status.Family;
+
+import org.apache.log4j.Logger;
+import org.ihtsdo.otf.refset.ValidationResult;
+import org.ihtsdo.otf.refset.helpers.ConfigUtility;
+import org.ihtsdo.otf.refset.jpa.RefsetJpa;
+import org.ihtsdo.otf.refset.jpa.TranslationJpa;
+import org.ihtsdo.otf.refset.jpa.ValidationResultJpa;
 import org.ihtsdo.otf.refset.jpa.services.rest.ValidationServiceRest;
+import org.ihtsdo.otf.refset.rf2.jpa.ConceptJpa;
+import org.ihtsdo.otf.refset.rf2.jpa.SimpleRefSetMemberJpa;
+
 
 /**
  * A client for connecting to a validation REST service.
@@ -13,7 +32,6 @@ import org.ihtsdo.otf.refset.jpa.services.rest.ValidationServiceRest;
 public class ValidationClientRest implements ValidationServiceRest {
 
   /** The config. */
-  @SuppressWarnings("unused")
   private Properties config = null;
 
   /**
@@ -25,6 +43,122 @@ public class ValidationClientRest implements ValidationServiceRest {
     this.config = config;
   }
 
-  // TODO: needs implO
+  /* see superclass */
+  @Override
+  public ValidationResult validateConcept(ConceptJpa member, String authToken)
+    throws Exception {
+    Client client = ClientBuilder.newClient();
+    WebTarget target =
+        client.target(config.getProperty("base.url") + "/validate/member");
+
+    String memberString =
+        ConfigUtility.getStringForGraph(member == null ? new ConceptJpa()
+        : member);
+    Logger.getLogger(getClass()).info(memberString);
+    Response response =
+        target.request(MediaType.APPLICATION_XML)
+            .header("Authorization", authToken).post(Entity.xml(memberString));
+
+    String resultString = response.readEntity(String.class);
+    if (response.getStatusInfo().getFamily() == Family.SUCCESSFUL) {
+      Logger.getLogger(getClass()).debug(resultString);
+    } else {
+      throw new Exception(resultString);
+    }
+
+    // converting to object
+    ValidationResult result =
+        (ValidationResult) ConfigUtility.getGraphForString(resultString,
+            ValidationResultJpa.class);
+    return result;
+  }
+
+  @Override
+  public ValidationResult validateRefset(RefsetJpa refset, String authToken)
+    throws Exception {
+    Client client = ClientBuilder.newClient();
+    WebTarget target =
+        client.target(config.getProperty("base.url") + "/validate/refset");
+
+    String refsetString =
+        ConfigUtility.getStringForGraph(refset == null ? new RefsetJpa()
+        : refset);
+    Logger.getLogger(getClass()).info(refsetString);
+    Response response =
+        target.request(MediaType.APPLICATION_XML)
+            .header("Authorization", authToken).post(Entity.xml(refsetString));
+
+    String resultString = response.readEntity(String.class);
+    if (response.getStatusInfo().getFamily() == Family.SUCCESSFUL) {
+      Logger.getLogger(getClass()).debug(resultString);
+    } else {
+      throw new Exception(resultString);
+    }
+
+    // converting to object
+    ValidationResult result =
+        (ValidationResult) ConfigUtility.getGraphForString(resultString,
+            ValidationResultJpa.class);
+    return result;
+  }
+
+  @Override
+  public ValidationResult validateTranslation(TranslationJpa translation,
+    String authToken) throws Exception {
+    Client client = ClientBuilder.newClient();
+    WebTarget target =
+        client.target(config.getProperty("base.url") + "/validate/translation");
+
+    String translationString =
+        ConfigUtility.getStringForGraph(translation == null ? new TranslationJpa()
+        : translation);
+    Logger.getLogger(getClass()).info(translationString);
+    Response response =
+        target.request(MediaType.APPLICATION_XML)
+            .header("Authorization", authToken).post(Entity.xml(translationString));
+
+    String resultString = response.readEntity(String.class);
+    if (response.getStatusInfo().getFamily() == Family.SUCCESSFUL) {
+      Logger.getLogger(getClass()).debug(resultString);
+    } else {
+      throw new Exception(resultString);
+    }
+
+    // converting to object
+    ValidationResult result =
+        (ValidationResult) ConfigUtility.getGraphForString(resultString,
+            ValidationResultJpa.class);
+    return result;
+  }
+
+  @Override
+  public ValidationResult validateSimpleRefSetMember(
+    SimpleRefSetMemberJpa member, String authToken) throws Exception {
+    Client client = ClientBuilder.newClient();
+    WebTarget target =
+        client.target(config.getProperty("base.url") + "/validate/member");
+
+    String memberString =
+        ConfigUtility.getStringForGraph(member == null ? new SimpleRefSetMemberJpa()
+        : member);
+    Logger.getLogger(getClass()).info(memberString);
+    Response response =
+        target.request(MediaType.APPLICATION_XML)
+            .header("Authorization", authToken).post(Entity.xml(memberString));
+
+    String resultString = response.readEntity(String.class);
+    if (response.getStatusInfo().getFamily() == Family.SUCCESSFUL) {
+      Logger.getLogger(getClass()).debug(resultString);
+    } else {
+      throw new Exception(resultString);
+    }
+
+    // converting to object
+    ValidationResult result =
+        (ValidationResult) ConfigUtility.getGraphForString(resultString,
+            ValidationResultJpa.class);
+    return result;
+  }
+
 
 }
