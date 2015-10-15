1b895a176db033ab30b899c0a597af11a8649ad6
bcarlsenca <bcarlsen@westcoastinformatics.com>
2015-10-14 19:13:17 -0700

    Add missing methods

--- rest-client/src/main/java/org/ihtsdo/otf/refset/rest/client/RefsetClientRest.java
+++ rest-client/src/main/java/org/ihtsdo/otf/refset/rest/client/RefsetClientRest.java
@@ -541,4 +541,18 @@ public class RefsetClientRest extends RootClientRest implements
 
   }
 
+  @Override
+  public Refset resumeRedefinition(Long refsetId, String newDefinition,
+    String authToken) throws Exception {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Refset resumeMigration(Long refsetId, String newTerminology,
+    String newVersion, String authToken) throws Exception {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
 }
