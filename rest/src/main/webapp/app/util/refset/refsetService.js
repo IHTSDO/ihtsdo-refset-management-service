633716a7f0cf91a79c48ec88cd724bbb3c761d4a
Tim Kao <timkao@gmail.com>
2015-10-20 14:44:13 -0500

    Spelling Correction

--- rest/src/main/webapp/app/util/refset/refsetService.js
+++ rest/src/main/webapp/app/util/refset/refsetService.js
@@ -54,6 +54,158 @@ tsApp.service('refsetService', [
         });
       return deferred.promise;
     }
+    
+    // get refset for id
+    this.getRefset = function(refsetId) {
+      console.debug("getRefset");
+      var deferred = $q.defer();
+
+      // get refset for id
+      gpService.increment()
+      $http.get(refsetUrl + "/" + refsetId).then(
+      // success
+      function(response) {
+        console.debug("  refset = ", response.data);
+        gpService.decrement();
+        deferred.resolve(response.data);
+      },
+      // error
+      function(response) {
+        utilService.handleError(response);
+        gpService.decrement();
+        deferred.reject(response.data);
+      });
+      return deferred.promise;
+    }
+    
+    // get refsets for project
+    this.getRefsetsForProject = function(projectId) {
+      console.debug("getRefsetsForProject");
+      var deferred = $q.defer();
+
+      // get refset for project
+      gpService.increment()
+      $http.get(refsetUrl + 'refsets' + "/" + projectId).then(
+      // success
+      function(response) {
+        console.debug("  projects = ", response.data);
+        gpService.decrement();
+        deferred.resolve(response.data);
+      },
+      // error
+      function(response) {
+        utilService.handleError(response);
+        gpService.decrement();
+        deferred.reject(response.data);
+      });
+      return deferred.promise;
+    }
+    
+    // Finds refsets based on pfs parameter and query
+    this.findRefsetsForQuery = function(queryStr, pfs) {
+
+      var query = (queryStr == null) ? "" : queryStr;
+      console.debug("findRefsetsForQuery", query, pfs);
+      // Setup deferred
+      var deferred = $q.defer();
+
+      // Make POST call
+      gpService.increment();
+      $http.post(refsetUrl + "refsets" + "?query=" + query, pfs).then(
+      // success
+      function(response) {
+        console.debug("  output = ", response.data);
+        gpService.decrement();
+        deferred.resolve(response.data);
+      },
+      // error
+      function(response) {
+        utilService.handleError(response);
+        gpService.decrement();
+        deferred.reject(response.data);
+      });
+      return deferred.promise;
+    }
+    
+    // add refset
+    this.addRefset = function(refset) {
+      console.debug("addRefset");
+      var deferred = $q.defer();
+
+      // Add refset
+      gpService.increment()
+      $http.put(refsetUrl + 'add', refset).then(
+      // success
+      function(response) {
+        console.debug("  refset = ", response.data);
+        gpService.decrement();
+        deferred.resolve(response.data);
+      },
+      // error
+      function(response) {
+        utilService.handleError(response);
+        gpService.decrement();
+        deferred.reject(response.data);
+      });
+      return deferred.promise;
+    }
+
+    // update refset
+    this.updateRefset = function(refset) {
+      console.debug("updateRefset");
+      var deferred = $q.defer();
+
+      // update refset
+      gpService.increment()
+      $http.post(refsetUrl + 'update', refset).then(
+      // success
+      function(response) {
+        console.debug("  refset = ", response.data);
+        gpService.decrement();
+        deferred.resolve(response.data);
+      },
+      // error
+      function(response) {
+        utilService.handleError(response);
+        gpService.decrement();
+        deferred.reject(response.data);
+      });
+      return deferred.promise;
+    }
+
+    // remove refset
+    this.removeRefset = function(refsetId) {
+      console.debug("removeRefset");
+      var deferred = $q.defer();
+
+      // remove refset
+      gpService.increment()
+      $http['delete'](refsetUrl + 'remove' + "/" + refsetId).then(
+      // success
+      function(response) {
+        console.debug("  refset = ", response.data);
+        gpService.decrement();
+        deferred.resolve(response.data);
+      },
+      // error
+      function(response) {
+        utilService.handleError(response);
+        gpService.decrement();
+        deferred.reject(response.data);
+      });
+      return deferred.promise;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     // find members for refset
     this.findRefsetMembersForQuery = function(refsetId, query, pfs) {
@@ -130,146 +282,13 @@ tsApp.service('refsetService', [
       return deferred.promise;
     }
 
-    // get refset for id
-    this.getRefset = function(refsetId) {
-      console.debug("getRefset");
-      var deferred = $q.defer();
 
-      // get refset for id
-      gpService.increment()
-      $http.get(refsetUrl + "/" + refsetId).then(
-      // success
-      function(response) {
-        console.debug("  refset = ", response.data);
-        gpService.decrement();
-        deferred.resolve(response.data);
-      },
-      // error
-      function(response) {
-        utilService.handleError(response);
-        gpService.decrement();
-        deferred.reject(response.data);
-      });
-      return deferred.promise;
-    }
 
-    // get refsets for project
-    this.getRefsetsForProject = function(projectId) {
-      console.debug("getRefsetsForProject");
-      var deferred = $q.defer();
 
-      // get refset for project
-      gpService.increment()
-      $http.get(refsetUrl + 'refsets' + "/" + projectId).then(
-      // success
-      function(response) {
-        console.debug("  projects = ", response.data);
-        gpService.decrement();
-        deferred.resolve(response.data);
-      },
-      // error
-      function(response) {
-        utilService.handleError(response);
-        gpService.decrement();
-        deferred.reject(response.data);
-      });
-      return deferred.promise;
-    }
 
-    // Finds refsets based on pfs parameter and query
-    this.findRefsetsForQuery = function(queryStr, pfs) {
 
-      var query = (queryStr == null) ? "" : queryStr;
-      console.debug("findRefsetsForQuery", query, pfs);
-      // Setup deferred
-      var deferred = $q.defer();
 
-      // Make POST call
-      gpService.increment();
-      $http.post(refsetUrl + "refsets" + "?query=" + query, pfs).then(
-      // success
-      function(response) {
-        console.debug("  output = ", response.data);
-        gpService.decrement();
-        deferred.resolve(response.data);
-      },
-      // error
-      function(response) {
-        utilService.handleError(response);
-        gpService.decrement();
-        deferred.reject(response.data);
-      });
-      return deferred.promise;
-    }
-
-    // add refset
-    this.addRefset = function(refset) {
-      console.debug("addRefset");
-      var deferred = $q.defer();
-
-      // Add refset
-      gpService.increment()
-      $http.put(refsetUrl + 'add', refset).then(
-      // success
-      function(response) {
-        console.debug("  refset = ", response.data);
-        gpService.decrement();
-        deferred.resolve(response.data);
-      },
-      // error
-      function(response) {
-        utilService.handleError(response);
-        gpService.decrement();
-        deferred.reject(response.data);
-      });
-      return deferred.promise;
-    }
-
-    // update refset
-    this.updateRefset = function(refset) {
-      console.debug("updateRefset");
-      var deferred = $q.defer();
-
-      // update refset
-      gpService.increment()
-      $http.post(refsetUrl + 'update', refset).then(
-      // success
-      function(response) {
-        console.debug("  refset = ", response.data);
-        gpService.decrement();
-        deferred.resolve(response.data);
-      },
-      // error
-      function(response) {
-        utilService.handleError(response);
-        gpService.decrement();
-        deferred.reject(response.data);
-      });
-      return deferred.promise;
-    }
-
-    // remove refset
-    this.removeRefset = function(refsetId) {
-      console.debug("removeRefset");
-      var deferred = $q.defer();
-
-      // remove refset
-      gpService.increment()
-      $http['delete'](refsetUrl + 'remove' + "/" + refsetId).then(
-      // success
-      function(response) {
-        console.debug("  refset = ", response.data);
-        gpService.decrement();
-        deferred.resolve(response.data);
-      },
-      // error
-      function(response) {
-        utilService.handleError(response);
-        gpService.decrement();
-        deferred.reject(response.data);
-      });
-      return deferred.promise;
-    }
+   
 
     // Initialize user role - only when refset service loads
     projectService.getUserHasAnyRole();
