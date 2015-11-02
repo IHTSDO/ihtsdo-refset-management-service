be27749b039f2f36babe4e9cfa99bd782a30f0f7
Tun Tun Naing <tnaing@westcoastinformatics.com>
2015-11-01 20:48:59 -0800

    implement reset release test.

---
+++ integration-test/src/test/java/org/ihtsdo/otf/refset/test/rest/RefsetReleaseTest.java
@@ -0,0 +1,304 @@
+/*
+ * Copyright 2015 West Coast Informatics, LLC
+ */
+/*
+ * 
+ */
+package org.ihtsdo.otf.refset.test.rest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.ihtsdo.otf.refset.Project;
+import org.ihtsdo.otf.refset.Refset;
+import org.ihtsdo.otf.refset.Refset.FeedbackEvent;
+import org.ihtsdo.otf.refset.User;
+import org.ihtsdo.otf.refset.ValidationResult;
+import org.ihtsdo.otf.refset.helpers.ConfigUtility;
+import org.ihtsdo.otf.refset.jpa.RefsetJpa;
+import org.ihtsdo.otf.refset.rest.client.ProjectClientRest;
+import org.ihtsdo.otf.refset.rest.client.RefsetClientRest;
+import org.ihtsdo.otf.refset.rest.client.ReleaseClientRest;
+import org.ihtsdo.otf.refset.rest.client.SecurityClientRest;
+import org.ihtsdo.otf.refset.rest.client.ValidationClientRest;
+import org.ihtsdo.otf.refset.workflow.WorkflowStatus;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Test case for redefinition.
+ */
+public class RefsetReleaseTest {
+
+  /** The viewer auth token. */
+  private static String viewerAuthToken;
+
+  /** The admin auth token. */
+  private static String adminAuthToken;
+
+  /** The service. */
+  protected static RefsetClientRest refsetService;
+
+  /** The release service. */
+  protected static ReleaseClientRest releaseService;
+
+  /** The security service. */
+  protected static SecurityClientRest securityService;
+
+  /** The validation service. */
+  protected static ValidationClientRest validationService;
+
+  /** The project service. */
+  protected static ProjectClientRest projectService;
+
+  /** The properties. */
+  protected static Properties properties;
+
+  /** The test username. */
+  protected static String testUser;
+
+  /** The test password. */
+  protected static String testPassword;
+
+  /** The test admin username. */
+  protected static String adminUser;
+
+  /** The test admin password. */
+  protected static String adminPassword;
+
+  /**
+   * Create test fixtures for class.
+   *
+   * @throws Exception the exception
+   */
+  @BeforeClass
+  public static void setupClass() throws Exception {
+
+    // instantiate properties
+    properties = ConfigUtility.getConfigProperties();
+
+    // instantiate required services
+    refsetService = new RefsetClientRest(properties);
+    releaseService = new ReleaseClientRest(properties);
+    securityService = new SecurityClientRest(properties);
+    validationService = new ValidationClientRest(properties);
+    projectService = new ProjectClientRest(properties);
+
+    // test run.config.ts has viewer user
+    testUser = properties.getProperty("viewer.user");
+    testPassword = properties.getProperty("viewer.password");
+
+    // test run.config.ts has admin user
+    adminUser = properties.getProperty("admin.user");
+    adminPassword = properties.getProperty("admin.password");
+
+    if (testUser == null || testUser.isEmpty()) {
+      throw new Exception("Test prerequisite: viewer.user must be specified");
+    }
+    if (testPassword == null || testPassword.isEmpty()) {
+      throw new Exception(
+          "Test prerequisite: viewer.password must be specified");
+    }
+    if (adminUser == null || adminUser.isEmpty()) {
+      throw new Exception("Test prerequisite: admin.user must be specified");
+    }
+    if (adminPassword == null || adminPassword.isEmpty()) {
+      throw new Exception("Test prerequisite: admin.password must be specified");
+    }
+
+  }
+
+  /**
+   * Create test fixtures per test.
+   *
+   * @throws Exception the exception
+   */
+  @SuppressWarnings("static-method")
+  @Before
+  public void setup() throws Exception {
+
+    // authentication
+    adminAuthToken =
+        securityService.authenticate(adminUser, adminPassword).getAuthToken();
+
+  }
+
+  /**
+   * Teardown.
+   *
+   * @throws Exception the exception
+   */
+  @SuppressWarnings("static-method")
+  @After
+  public void teardown() throws Exception {
+
+    // logout
+    securityService.logout(viewerAuthToken);
+    securityService.logout(adminAuthToken);
+  }
+
+  /**
+   * Make refset.
+   *
+   * @param name the name
+   * @param definition the definition
+   * @param type the type
+   * @param project the project
+   * @param refsetId the refset id
+   * @param auth the auth
+   * @return the refset jpa
+   * @throws Exception the exception
+   */
+  private RefsetJpa makeRefset(String name, String definition,
+    Refset.Type type, Project project, String refsetId, User auth)
+    throws Exception {
+    RefsetJpa refset = new RefsetJpa();
+    refset.setActive(true);
+    refset.setType(type);
+    refset.setName(name);
+    refset.setDescription("Description of refset " + name);
+    refset.setDefinition(definition);
+    refset.setExternalUrl(null);
+    refset.setFeedbackEmail("***REMOVED***");
+    refset.getEnabledFeedbackEvents().add(FeedbackEvent.MEMBER_ADD);
+    refset.getEnabledFeedbackEvents().add(FeedbackEvent.MEMBER_REMOVE);
+    refset.setForTranslation(false);
+    refset.setLastModified(new Date());
+    refset.setModuleId("900000000000445007");
+    refset.setProject(project);
+    refset.setPublishable(true);
+    refset.setPublished(true);
+    refset.setTerminology("SNOMEDCT");
+    refset.setTerminologyId(refsetId);
+    // This is an opportunity to use "branch"
+    refset.setVersion("2015-01-31");
+    refset.setWorkflowPath("DFEAULT");
+    refset.setWorkflowStatus(WorkflowStatus.PUBLISHED);
+
+    if (type == Refset.Type.EXTERNAL) {
+      refset.setExternalUrl("http://www.example.com/some/other/refset.txt");
+    }
+
+    // Validate refset
+    ValidationResult result =
+        validationService.validateRefset(refset, auth.getAuthToken());
+    if (!result.isValid()) {
+      Logger.getLogger(getClass()).error(result.toString());
+      throw new Exception("Refset does not pass validation.");
+    }
+    // Add refset
+
+    refset = (RefsetJpa) refsetService.addRefset(refset, auth.getAuthToken());
+
+    if (type == Refset.Type.EXTENSIONAL) {
+      // Import members (from file)
+      ValidationResult vr =
+          refsetService.beginImportMembers(refset.getId(), "DEFAULT",
+              auth.getAuthToken());
+      if (!vr.isValid()) {
+        throw new Exception("import staging is invalid - " + vr);
+      }
+      InputStream in =
+          new FileInputStream(
+              new File(
+                  "../config/src/main/resources/data/refset/der2_Refset_SimpleSnapshot_INT_20140731.txt"));
+      refsetService.finishImportMembers(null, in, refset.getId(), "DEFAULT",
+          auth.getAuthToken());
+      in.close();
+    } else if (type == Refset.Type.INTENSIONAL) {
+      // Import definition (from file)
+      InputStream in =
+          new FileInputStream(
+              new File(
+                  "../config/src/main/resources/data/refset/der2_Refset_DefinitionSnapshot_INT_20140731.txt"));
+      refsetService.importDefinition(null, in, refset.getId(), "DEFAULT",
+          auth.getAuthToken());
+      in.close();
+    }
+
+    return refset;
+  }
+  
+  /**
+   * Test refset release including begin and cancel.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testRelease001() throws Exception {
+    Logger.getLogger(getClass()).debug("RUN testMigration001");
+
+    Project project2 = projectService.getProject(2L, adminAuthToken);
+    User admin = securityService.authenticate(adminUser, adminPassword);
+    // Create refset (intensional) and import definition
+    RefsetJpa refset1 =
+        makeRefset("refset99", "<<259866009 |Malaria antibody (substance)|",
+            Refset.Type.INTENSIONAL, project2, null, admin);
+    // Begin release
+    releaseService.beginRefsetRelease(refset1.getId(), ConfigUtility.DATE_FORMAT.format(Calendar.getInstance()), adminAuthToken);
+    // Cancel release
+    releaseService.cancelRefsetRelease(refset1.getId(), adminAuthToken);
+    // clean up
+    refsetService.removeRefset(refset1.getId(), true, adminAuthToken);
+  }
+
+  /**
+   * Test refset release including begin, validate and cancel.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testRelease002() throws Exception {
+    Logger.getLogger(getClass()).debug("RUN testMigration001");
+
+    Project project2 = projectService.getProject(2L, adminAuthToken);
+    User admin = securityService.authenticate(adminUser, adminPassword);
+    // Create refset (intensional) and import definition
+    RefsetJpa refset1 =
+        makeRefset("refset99", "<<259866009 |Malaria antibody (substance)|",
+            Refset.Type.INTENSIONAL, project2, null, admin);
+    // Begin release
+    releaseService.beginRefsetRelease(refset1.getId(), ConfigUtility.DATE_FORMAT.format(Calendar.getInstance()), adminAuthToken);
+    // Validate release
+    releaseService.validateRefsetRelease(refset1.getId(), adminAuthToken);
+    // Cancel release
+    releaseService.cancelRefsetRelease(refset1.getId(), adminAuthToken);
+    // clean up
+    refsetService.removeRefset(refset1.getId(), true, adminAuthToken);
+  }
+
+  /**
+   * Test refset release including begin, validate, preview and cancel.
+   *
+   * @throws Exception the exception
+   */
+  @Test
+  public void testRelease003() throws Exception {
+    Logger.getLogger(getClass()).debug("RUN testMigration001");
+
+    Project project2 = projectService.getProject(2L, adminAuthToken);
+    User admin = securityService.authenticate(adminUser, adminPassword);
+    // Create refset (intensional) and import definition
+    RefsetJpa refset1 =
+        makeRefset("refset99", "<<259866009 |Malaria antibody (substance)|",
+            Refset.Type.INTENSIONAL, project2, null, admin);
+    // Begin release
+    releaseService.beginRefsetRelease(refset1.getId(), ConfigUtility.DATE_FORMAT.format(Calendar.getInstance()), adminAuthToken);
+    // Validate release
+    releaseService.validateRefsetRelease(refset1.getId(), adminAuthToken);
+    // Preview release
+    releaseService.previewRefsetRelease(refset1.getId(), "DEFAULT", adminAuthToken);
+    // Cancel release
+    releaseService.cancelRefsetRelease(refset1.getId(), adminAuthToken);
+    // clean up
+    refsetService.removeRefset(refset1.getId(), true, adminAuthToken);
+  }
+
+}
