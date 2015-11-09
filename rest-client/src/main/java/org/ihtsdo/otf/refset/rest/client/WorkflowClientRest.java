603eb90f14d585340696b763ea2423532f7617df
Deborah Shapiro <deborah.seider.shapiro@gmail.com>
2015-11-09 13:12:44 -0800

    Service placeholders for WorkflowClientRest.java.

--- rest-client/src/main/java/org/ihtsdo/otf/refset/rest/client/WorkflowClientRest.java
+++ rest-client/src/main/java/org/ihtsdo/otf/refset/rest/client/WorkflowClientRest.java
@@ -26,7 +26,6 @@ import org.ihtsdo.otf.refset.jpa.services.rest.WorkflowServiceRest;
 import org.ihtsdo.otf.refset.rf2.jpa.ConceptJpa;
 import org.ihtsdo.otf.refset.worfklow.TrackingRecordJpa;
 import org.ihtsdo.otf.refset.workflow.TrackingRecord;
-import org.ihtsdo.otf.refset.workflow.TrackingRecordList;
 
 /**
  * Client for connecting to a workflow REST service.
@@ -419,6 +418,20 @@ public class WorkflowClientRest extends RootClientRest implements
   @Override
   public TrackingRecord getTrackingRecordForRefset(Long refsetId,
     String authToken) throws Exception {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public RefsetList findAllAvailableRefsets(Long projectId, String userName,
+    PfsParameterJpa pfs, String authToken) throws Exception {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public RefsetList findAllAssignedRefsets(Long projectId, String userName,
+    PfsParameterJpa pfs, String authToken) throws Exception {
     // TODO Auto-generated method stub
     return null;
   }
