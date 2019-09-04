f5b1ce62bace8ad921262b117e4c822a083c7317
Nuno Marques <nuno.marques@gmail.com>
2019-09-04 13:12:13 -0700

    Clean up unused import.

--- jpa-model/src/main/java/org/ihtsdo/otf/refset/rf2/jpa/ConceptRefsetMemberJpa.java
+++ jpa-model/src/main/java/org/ihtsdo/otf/refset/rf2/jpa/ConceptRefsetMemberJpa.java
@@ -12,7 +12,6 @@ import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
