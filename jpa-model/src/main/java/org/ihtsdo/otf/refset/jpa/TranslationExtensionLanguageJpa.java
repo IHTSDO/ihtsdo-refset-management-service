bbf4d8bf926e3c0a04c179e8a23ca4a829e3ee6c
Nuno Marques <nuno.marques@gmail.com>
2019-09-12 14:03:01 -0700

    Remove unused imports

--- jpa-model/src/main/java/org/ihtsdo/otf/refset/jpa/TranslationExtensionLanguageJpa.java
+++ jpa-model/src/main/java/org/ihtsdo/otf/refset/jpa/TranslationExtensionLanguageJpa.java
@@ -5,7 +5,6 @@ package org.ihtsdo.otf.refset.jpa;
 
 import java.util.Date;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,17 +12,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import javax.persistence.TableGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
 import org.hibernate.envers.Audited;
 import org.hibernate.search.annotations.Analyze;
-import org.hibernate.search.annotations.ContainedIn;
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.FieldBridge;
 import org.hibernate.search.annotations.Index;
