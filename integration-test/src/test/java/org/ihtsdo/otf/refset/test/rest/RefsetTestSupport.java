ba893d2a51c99c352123a5c7f35acba6cfce1120
Nuno Marques <nuno.marques@gmail.com>
2019-09-19 08:34:31 -0700

    Clean up unsed imports.

--- integration-test/src/test/java/org/ihtsdo/otf/refset/test/rest/RefsetTestSupport.java
+++ integration-test/src/test/java/org/ihtsdo/otf/refset/test/rest/RefsetTestSupport.java
@@ -7,7 +7,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
