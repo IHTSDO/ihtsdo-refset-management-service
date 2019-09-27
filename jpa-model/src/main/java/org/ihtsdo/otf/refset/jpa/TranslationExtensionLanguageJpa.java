1616ad88e68ac38af9f40640eb211798d13d58f8
Nuno Marques <nuno.marques@gmail.com>
2019-09-27 11:43:29 -0700

    Remove unused imports.

--- jpa-model/src/main/java/org/ihtsdo/otf/refset/jpa/TranslationExtensionLanguageJpa.java
+++ jpa-model/src/main/java/org/ihtsdo/otf/refset/jpa/TranslationExtensionLanguageJpa.java
@@ -3,8 +3,6 @@
  */
 package org.ihtsdo.otf.refset.jpa;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,8 +11,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.TableGenerator;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
