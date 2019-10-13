52a0637a63f94856873c0c82325c83c40cc9b3e7
Rick Wood <rwood@westcoastinformatics.com>
2019-10-13 09:02:03 -0700

    JDK11-82: Handling Snowstorm inactive concepts that have no PT.

--- jpa-services/src/main/java/org/ihtsdo/otf/refset/jpa/services/handlers/SnowstormTerminologyHandler.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/refset/jpa/services/handlers/SnowstormTerminologyHandler.java
@@ -1070,7 +1070,9 @@ public class SnowstormTerminologyHandler extends AbstractTerminologyHandler {
 
         final Description desc = new DescriptionJpa();
         desc.setActive(entry.get("active").asText().equals("true"));
-        desc.setTerm(pt.get("term").asText());
+        String term = pt == null ? ""
+            : pt.get("term") == null ? "" : pt.get("term").asText();        
+        desc.setTerm(term);
         if (conceptMap.containsKey(conceptId)) {
           final Concept concept = conceptMap.get(conceptId);
           if (desc.isActive() || !localPfs.getActiveOnly()) {
@@ -1092,7 +1094,7 @@ public class SnowstormTerminologyHandler extends AbstractTerminologyHandler {
                 .setDefinitionStatusId(entry.get("definitionStatus").asText());
             concept.setTerminologyId(conceptId);
             concept.setModuleId(entry.get("moduleId").asText());
-            String term = fsn == null ? ""
+            term = fsn == null ? ""
                 : fsn.get("term") == null ? "" : fsn.get("term").asText();
             concept.setName(term);
             concept.setPublishable(true);
