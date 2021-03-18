76365a96ad18884f0f7ce1a4b58e085f28082137
Richard Wood <rickawood@gmail.com>
2021-03-18 12:14:19 -0700

    Adding clause to avoid NPE

--- jpa-services/src/main/java/org/ihtsdo/otf/refset/jpa/services/handlers/DefaultWorkflowActionHandler.java
+++ jpa-services/src/main/java/org/ihtsdo/otf/refset/jpa/services/handlers/DefaultWorkflowActionHandler.java
@@ -1023,8 +1023,8 @@ public class DefaultWorkflowActionHandler implements WorkflowActionHandler {
     final ConceptListJpa list = new ConceptListJpa();
     int totalCount = 0;
     // No need to use applyPfsToList if there is not a filter
-    if (pfs != null && (pfs.getQueryRestriction() == null
-        || pfs.getQueryRestriction().isEmpty())) {
+    if ((pfs != null && (pfs.getQueryRestriction() == null
+        || pfs.getQueryRestriction().isEmpty()) || pfs==null)) {
 
       final Query ctQuery =
           ((RootServiceJpa) service).getEntityManager().createQuery(
